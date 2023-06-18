7ecc2678314bc416ce4651716946f944a3398ad1
anastasiadergaeva <adergaeva@gmail.com>
2023-06-18 12:37:04 +0300

    case switch exersize

--- index.js
+++ index.js
@@ -36,3 +36,16 @@ if (homeValue <= receivedMoney) {
 }
 
 console.log(receivedMoney);
+
+/* Методом prompt получите ответ пользователя на вопрос "Сколько будет 7 + или - 15?". Если ответ верен, выведите в консоли "Успех", если нет - "Вы робот!", а если он введёт "Я не робот", то тоже "Успех". */
+
+const questionForUser = prompt('Сколько будет 7 + или - 15?');
+switch (true) {
+    case Number(questionForUser) === 22:
+    case Number(questionForUser) === -8:
+    case questionForUser === 'Я не робот':
+        console.log('Успех!');
+        break;
+    default:
+        console.log('Вы робот!');
+}
