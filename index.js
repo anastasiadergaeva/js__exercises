b5d822349b329cb458ee6a4d04b6a7da28c40032
anastasiadergaeva <adergaeva@gmail.com>
2023-06-19 10:58:15 +0300

    add boolean

--- index.js
+++ index.js
@@ -49,3 +49,19 @@ switch (true) {
     default:
         console.log('Вы робот!');
 }
+
+// Пользователь хочет приобрести игру в магазине. Он может это сделать только если:
+// - Eго баланс больше 1000 (balance) или число бонусов больше 100 (bonusBalance)
+// - Он не забанен (isBanned)
+// - Игра не куплена (isExist)
+// - Игра в продаже (isSelling)
+// Напишите условие для покупки и выведите в консоль результат.
+
+const balance = 1500;
+const bonusBalance = 2110;
+const isBanned = false;
+const isExist = false;
+const isSelling = true;
+
+const canBuy = (balance > 1000 || bonusBalance > 100) && (!isBanned) && (!isExist) && isSelling;
+console.log(`Возможно ли купить игру: ${canBuy ? 'Возможно' : 'Невозможно'}`);
