c544f001c57e2a9346f6c738519ba9661fc7d151
anastasiadergaeva <adergaeva@gmail.com>
2023-06-26 10:20:48 +0300

    add arrow function

--- index.js
+++ index.js
@@ -65,3 +65,14 @@ const isSelling = true;
 
 const canBuy = (balance > 1000 || bonusBalance > 100) && (!isBanned) && (!isExist) && isSelling;
 console.log(`Возможно ли купить игру: ${canBuy ? 'Возможно' : 'Невозможно'}`);
+
+/** Переписать функцию в стрелочную */
+
+function toPower(num, power) {
+    const res = num ** power;
+    return res;
+}
+console.log(toPower(2, 3));
+
+const toPowerArrow = (num, power) => num ** power;
+console.log(toPowerArrow(2, 3));
