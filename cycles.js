92d451827d0dec85e72adf0e6aee6e6371084c90
anastasiadergaeva <adergaeva@gmail.com>
2023-07-21 20:09:26 +0300

    add balance counting

--- cycles.js
+++ cycles.js
@@ -7,3 +7,55 @@ for (let i = arr.length - 1; i >= 0; i--) {
 }
 
 console.log(resultArray.join(' '));
+
+// Расчёт итогового баланса
+
+const operations = [1000, -700, 300, -500, 10000];
+const startingBalance = 100;
+
+function getFinalBalance() {
+    let sumOfOperations = 0;
+    for (let i = 0; i < operations.length; i++) {
+        sumOfOperations += operations[i];
+    }
+    const finalBalance = sumOfOperations + startingBalance;
+    return finalBalance;
+}
+
+console.log(getFinalBalance());
+
+function getNegativeBalance(arrayOfOperations, initialBalance) {
+    let balance = initialBalance;
+    let positiveBalance = true;
+    for (let element of arrayOfOperations) {
+        balance += element;
+        switch (true) {
+            case balance < 0:
+                positiveBalance = false;
+                break;
+        }
+    }
+    return positiveBalance;
+}
+
+console.log(getNegativeBalance(operations, startingBalance));
+
+function getAverageIncomeAndExpense(arrayOfOperations) {
+    let positiveCount = 0;
+    let negativeCount = 0;
+    let positiveSum = 0;
+    let negativeSum = 0;
+    for (const element of arrayOfOperations) {
+        if (element > 0) {
+            positiveCount++;
+            positiveSum += element;
+        }
+        else if (element < 0) {
+            negativeCount++;
+            negativeSum += element;
+        }
+    }
+    return [positiveSum / positiveCount, negativeSum / negativeCount];
+}
+
+console.log(getAverageIncomeAndExpense(operations));
