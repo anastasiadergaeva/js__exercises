cc6eb295776c1716fb6ad93c251f5b526b3d0ae3
anastasiadergaeva <adergaeva@gmail.com>
2023-08-10 12:59:55 +0300

    some realization

--- arriterations.js
+++ arriterations.js
@@ -11,3 +11,14 @@ const averageValue = arr.reduce((acc, element) => {
     return acc + element / arr.length;
 }, 0);
 console.log(averageValue);
+
+// реализация some
+const counts = [2, 4, 4, 10, 20];
+function some (array, element) {
+    const result = counts.find(el => el === element);
+    return result == undefined ? false : true;
+}
+console.log(some(counts, 0));
+
+// или
+console.log(counts.some(el => el === 2));
