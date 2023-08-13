5d1bcf79e4bf146c2a645cfe65097acf67fd5e48
anastasiadergaeva <adergaeva@gmail.com>
2023-08-13 12:15:53 +0300

    highlight name surname

---
+++ workingwithstrings.js
@@ -0,0 +1,5 @@
+const fullUserName = 'Elena magiccity elf purple Volnova';
+
+const userName = fullUserName.slice(0, fullUserName.indexOf(' '));
+const userSurname = fullUserName.slice(fullUserName.lastIndexOf(' '), fullUserName.length);
+console.log(userName, userSurname);
