e1b5dfc5643af5e203e986731d5a7a156451aa65
anastasiadergaeva <adergaeva@gmail.com>
2023-09-03 19:23:48 +0300

    add objects

---
+++ objects.js
@@ -0,0 +1,9 @@
+// сортировка пользователей
+const users = [
+    { name: 'Вася', age: 30 },
+    { name: 'Катя', age: 18 },
+    { name: 'Аня', age: 40 },
+    { name: 'Петя', age: 25 },
+];
+
+console.log(users.sort((a, b) => a.age - b.age));
